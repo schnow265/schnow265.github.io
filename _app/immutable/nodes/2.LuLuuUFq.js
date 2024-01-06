d79ab898b36036d9333549dd7c0521702a83d1b1
schnow265 <thesnowbox@icloud.com>
2024-01-06 21:55:41 +0100

    Updates

---
+++ _app/immutable/nodes/2.LuLuuUFq.js
@@ -0,0 +1,7 @@
+import{s as r,n as s}from"../chunks/scheduler.k-kUyWhY.js";import{S as d,i as f,g as h,m as c,h as p,x as u,n as g,a as i,f as l}from"../chunks/index.3eaemhlc.js";function x(m){let e,a="Welcome",n;return{c(){e=h("h1"),e.textContent=a,n=c(`
+
+Please excuse this broken mess of a Svelte page. I am pretty new to this. And finding good fonts. 
+Don't tell me that my choices are bad. I will find this out soon enough, if it's really bad.`)},l(t){e=p(t,"H1",{"data-svelte-h":!0}),u(e)!=="svelte-4p3d56"&&(e.textContent=a),n=g(t,`
+
+Please excuse this broken mess of a Svelte page. I am pretty new to this. And finding good fonts. 
+Don't tell me that my choices are bad. I will find this out soon enough, if it's really bad.`)},m(t,o){i(t,e,o),i(t,n,o)},p:s,i:s,o:s,d(t){t&&(l(e),l(n))}}}class b extends d{constructor(e){super(),f(this,e,null,x,r,{})}}export{b as component};
